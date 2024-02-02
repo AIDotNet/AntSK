cedcebdfafc758539da95152b3d4474f64476b2b
zyxucp <286513187@qq.com>
2024-02-02 13:02:07 +0800

    add 默认路由

--- Xzy.KnowledgeBase/Pages/Account/Center/Components/AvatarList/AvatarList.razor.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Components;
-
-namespace Xzy.KnowledgeBase.Pages.Account.Center
-{
-    public partial class AvatarList
-    {
-        [Parameter] public RenderFragment ChildContent { get; set; }
-    }
-}
--- Xzy.KnowledgeBase/Pages/Form/AdvancedForm/AdvancedForm.razor.cs
+++
@@ -1,9 +0,0 @@
-using Xzy.KnowledgeBase.Models;
-
-namespace Xzy.KnowledgeBase.Pages.Form
-{
-    public partial class AdvancedForm
-    {
-        private readonly AdvancedFormModel _model = new AdvancedFormModel();
-    }
-}
