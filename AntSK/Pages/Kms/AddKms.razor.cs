95b99f0c1b64a2ee95dc9171ab196f741833ad8b
zyxucp <286513187@qq.com>
2024-02-04 11:32:19 +0800

    add 新增知识库

---
+++ AntSK/Pages/Kms/AddKms.razor.cs
@@ -0,0 +1,55 @@
+﻿using AntDesign;
+using Microsoft.AspNetCore.Components;
+using AntSK.Domain.Repositories;
+using AntSK.Models;
+
+namespace AntSK.Pages.Kms
+{
+    public class FormItemLayout
+    {
+        public ColLayoutParam LabelCol { get; set; }
+        public ColLayoutParam WrapperCol { get; set; }
+    }
+    public partial class AddKms
+    {
+        [Inject]
+        protected IKmss_Repositories _kmss_Repositories { get; set; }
+        [Inject]
+        protected NavigationManager NavigationManager { get; set; }
+
+        private readonly Kmss _kmsModel = new Kmss() {  ChatModel="gpt4-turbo",EmbeddingModel= "text-embedding-ada-002" };
+
+        private readonly FormItemLayout _formItemLayout = new FormItemLayout
+        {
+            LabelCol = new ColLayoutParam
+            {
+                Xs = new EmbeddedProperty { Span = 24 },
+                Sm = new EmbeddedProperty { Span = 7 },
+            },
+
+            WrapperCol = new ColLayoutParam
+            {
+                Xs = new EmbeddedProperty { Span = 24 },
+                Sm = new EmbeddedProperty { Span = 12 },
+                Md = new EmbeddedProperty { Span = 10 },
+            }
+        };
+        private readonly FormItemLayout _submitFormLayout = new FormItemLayout
+        {
+            WrapperCol = new ColLayoutParam
+            {
+                Xs = new EmbeddedProperty { Span = 24, Offset = 0 },
+                Sm = new EmbeddedProperty { Span = 10, Offset = 7 },
+            }
+        };
+
+        private void HandleSubmit()
+        {
+            _kmsModel.Id = Guid.NewGuid().ToString();
+            _kmss_Repositories.Insert(_kmsModel);
+
+            NavigationManager.NavigateTo("/kmslist");
+        }
+    }
+
+}
