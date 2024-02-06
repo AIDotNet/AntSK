a418d10e07cf252525e567af4cc962875e8f6089
zyxucp <286513187@qq.com>
2024-02-06 15:19:44 +0800

    add 增加应用配置界面

--- AntSK.Domain/Repositories/AI/App/Apps.cs
+++ AntSK.Domain/Repositories/AI/App/Apps.cs
@@ -39,5 +39,9 @@ namespace AntSK.Domain.Repositories
         /// 提示词
         /// </summary>
         public string? Prompt { get; set; }
+        /// <summary>
+        /// 知识库ID列表
+        /// </summary>
+        public string? KmsIdList { get; set; }
     }
 }
--- AntSK/Pages/AppPage/AddApp.razor.cs
+++ AntSK/Pages/AppPage/AddApp.razor.cs
@@ -9,11 +9,20 @@ namespace AntSK.Pages.AppPage
     public partial class AddApp
     {
         [Inject]
-        protected IApps_Repositories apps_Repositories { get; set; }
+        protected IApps_Repositories _apps_Repositories { get; set; }
+
+        [Inject]
+        protected IKmss_Repositories _kmss_Repositories { get; set; }
         [Inject]
         protected NavigationManager NavigationManager { get; set; }
 
-        private readonly Apps _appModel = new Apps() ;
+        private Apps _appModel = new Apps() ;
+
+        IEnumerable <string>  kmsIds;
+
+        private List<Kmss> _kmsList = new List<Kmss>();
+
+
         private string _errorMsg { get; set; }
 
         private readonly FormItemLayout _formItemLayout = new FormItemLayout
@@ -39,21 +48,36 @@ namespace AntSK.Pages.AppPage
                 Sm = new EmbeddedProperty { Span = 10, Offset = 7 },
             }
         };
-
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+            _kmsList = _kmss_Repositories.GetList();
+        }
         private void HandleSubmit()
         {
             _appModel.Id = Guid.NewGuid().ToString();
-            if (apps_Repositories.IsAny(p => p.Name == _appModel.Name))
+            if (_apps_Repositories.IsAny(p => p.Name == _appModel.Name))
             {
                 _errorMsg = "名称已存在！";
                 return;
             }
 
-            apps_Repositories.Insert(_appModel);
+            if (kmsIds.Count() > 0)
+            {
+                _appModel.KmsIdList = string.Join(",", kmsIds);
+            }
+
+            _apps_Repositories.Insert(_appModel);
 
             //NavigationManager.NavigateTo($"/app/detail/{_appModel.Id}");
             NavigationManager.NavigateTo($"/applist");
         }
+
+        private void OnSelectedItemChangedHandler(string value)
+        {
+            Console.WriteLine($"selected: ${value}");
+        }
+
     }
-    
+
 }
