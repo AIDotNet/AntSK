2ad3953d3f19863151d89f2fc2914804c4ba19da
zyxucp <286513187@qq.com>
2024-03-08 00:28:27 +0800

    add session

---
+++ src/AntSK/Models/UserSession.cs
@@ -0,0 +1,8 @@
+﻿namespace AntSK.Models
+{
+    public class UserSession
+    {
+        public string UserName { get; set; }
+        public string Role { get; set; }
+    }
+}
--- src/AntSK/Services/Auth/AntSKAuthProvider .cs
+++ src/AntSK/Services/Auth/AntSKAuthProvider .cs
@@ -1,13 +1,18 @@
 ﻿using AntSK.Domain.Options;
 using AntSK.Domain.Repositories;
 using AntSK.Domain.Utils;
+using AntSK.Models;
 using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 using System.Security.Claims;
 using System.Security.Principal;
 
 namespace AntSK.Services.Auth
 {
-    public class AntSKAuthProvider(IUsers_Repositories _users_Repositories) : AuthenticationStateProvider
+    public class AntSKAuthProvider(
+        IUsers_Repositories _users_Repositories,
+        ProtectedSessionStorage _protectedSessionStore
+        ) : AuthenticationStateProvider
     {
         private ClaimsIdentity identity = new ClaimsIdentity();
 
@@ -21,6 +26,7 @@ namespace AntSK.Services.Auth
                 // 管理员认证成功，创建用户的ClaimsIdentity
                 var claims = new[] { new Claim(ClaimTypes.Name, username) };
                 identity = new ClaimsIdentity(claims, "AntSKAdmin");
+                await _protectedSessionStore.SetAsync("UserSession", new UserSession() { UserName = username, Role = "AntSKAdmin" });
                 NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
                 return true;
             }
@@ -37,6 +43,7 @@ namespace AntSK.Services.Auth
                 // 用户认证成功，创建用户的ClaimsIdentity
                 var claims = new[] { new Claim(ClaimTypes.Name, username) };
                 identity = new ClaimsIdentity(claims, "AntSKUser");
+                await _protectedSessionStore.SetAsync("UserSession", new UserSession() { UserName = username, Role = "AntSKUser" });
                 NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
                 return true;
             }
@@ -47,10 +54,11 @@ namespace AntSK.Services.Auth
             var user = new ClaimsPrincipal(identity);
             return user;
         }
-        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
         {
+            var userSession = await _protectedSessionStore.GetAsync<UserSession>("UserSession");
             var user = new ClaimsPrincipal(identity);
-            return Task.FromResult(new AuthenticationState(user));
+            return await Task.FromResult(new AuthenticationState(user));
         }
     }
 }
