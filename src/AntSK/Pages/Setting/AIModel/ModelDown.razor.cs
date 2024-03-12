8a9609544853736ba501c40401e32cf637d426e6
zyxucp <286513187@qq.com>
2024-03-12 21:57:34 +0800

    add 增加模型下载地址

---
+++ src/AntSK/Pages/Setting/AIModel/ModelDown.razor.cs
@@ -0,0 +1,29 @@
+﻿using AntDesign;
+using AntSK.Models;
+using AntSK.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace AntSK.Pages.Setting.AIModel
+{
+    public partial class ModelDown
+    {
+        private readonly ListFormModel _model = new ListFormModel();
+        private readonly IList<string> _selectCategories = new List<string>();
+
+        private IList<ListItemDataType> _fakeList = new List<ListItemDataType>();
+
+
+        [Inject] public IProjectService ProjectService { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+            _fakeList = await ProjectService.GetFakeListAsync(8);
+        }
+
+        private async Task Search(string searchKey)
+        {
+            
+        }
+    }
+}
