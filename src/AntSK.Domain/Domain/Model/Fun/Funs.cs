b5e527afdbb390e7743f753d682bf2a438ccfb90
zyxucp <286513187@qq.com>
2024-03-15 22:33:39 +0800

    add 增加函数列表

---
+++ src/AntSK.Domain/Domain/Model/Fun/Funs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntSK.Domain.Domain.Model.Fun
+{
+    public class Funs
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ src/AntSK/Pages/Plugin/FunPage/FunList.razor.cs
@@ -0,0 +1,60 @@
+﻿using AntDesign;
+using AntSK.Domain.Domain.Model.Fun;
+using AntSK.Domain.Domain.Service;
+using AntSK.Domain.Repositories;
+using Microsoft.AspNetCore.Components;
+using Microsoft.SemanticKernel;
+
+namespace AntSK.Pages.ApiPage
+{
+    public partial class FunList
+    {
+        private Funs[] _data = { };
+
+        [Inject]
+        FunctionService _functionService { get; set; }
+        [Inject]
+        IServiceProvider _serviceProvider { get; set; }
+        [Inject]
+        IConfirmService _confirmService { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+            await InitData("");
+        }
+
+        private async Task InitData(string searchKey)
+        {
+            var list = new List<Funs> { new Funs() };
+
+            _functionService.SearchMarkedMethods();
+            using var scope = _serviceProvider.CreateScope();
+
+            var funList = _functionService.Functions;
+            if (!string.IsNullOrEmpty(searchKey))
+            {
+                funList = funList.Where(x => x.Key.Contains(searchKey)).ToDictionary(x => x.Key, x => x.Value);
+            }
+            foreach (var func in funList)
+            {
+
+                var methodInfo = _functionService.MethodInfos[func.Key];
+                list.Add(new Funs() { Name = func.Key, Description = methodInfo.Description });
+            }
+            _data = list.ToArray();
+            await InvokeAsync(StateHasChanged);
+        }
+
+        private void NavigateToAddApp()
+        {
+            NavigationManager.NavigateTo("/plugins/fun/add");
+        }
+
+        private async Task Search(string searchKey)
+        {
+            await InitData(searchKey);
+        }
+    
+    }
+}
