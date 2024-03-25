c286258f2b4a595564fb3e06546afbf6db3b0904
zeyu xu <286513187@qq.com>
2024-03-25 23:04:17 +0800

    del 删除LLamaSharp早起http版本

--- src/AntSK.Domain/Options/LLamaSharpOption.cs
+++ src/AntSK.Domain/Options/LLamaSharpOption.cs
@@ -3,10 +3,6 @@
     public class LLamaSharpOption
     {
         public static string RunType { get; set; }
-        public static string Chat { get; set; }
-
-        public static string Embedding { get; set; }
-
         public static string FileDirectory { get; set; } = Directory.GetCurrentDirectory();
     }
 }
