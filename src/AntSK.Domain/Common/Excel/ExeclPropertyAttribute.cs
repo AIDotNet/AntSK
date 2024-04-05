85d1c5ea7ec820da83ed4b6e396f309c559d428c
zyxucp <286513187@qq.com>
2024-04-05 19:17:49 +0800

    add npoi

---
+++ src/AntSK.Domain/Common/Excel/ExeclPropertyAttribute.cs
@@ -0,0 +1,28 @@
+﻿using NPOI.SS.UserModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AntSK.Domain
+{
+    public class ExeclPropertyAttribute : Attribute
+    {
+        public ExeclPropertyAttribute()
+        {
+
+        }
+        public ExeclPropertyAttribute(string displayName, int order, CellType cellType = CellType.String)
+        {
+            DisplayName = displayName;
+            Order = order;
+            CellType = cellType;
+        }
+
+        public string DisplayName { get; set; }
+
+        public int Order { get; set; }
+
+        public CellType CellType { get; set; }
+    }
+}
