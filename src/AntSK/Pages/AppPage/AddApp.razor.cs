ad949681dd002ed9f9f8b77e2e0531f310399864
zeyu xu <286513187@qq.com>
2024-04-05 14:41:58 +0800

    add change

--- src/AntSK/Pages/AppPage/AddApp.razor.cs
+++ src/AntSK/Pages/AppPage/AddApp.razor.cs
@@ -135,6 +135,35 @@ namespace AntSK.Pages.AppPage
         {
             NavigationManager.NavigateTo("/setting/modellist");
         }
+
+        private void OnAppTypeChange(string value)
+        {
+            if (value == "kms"&& string.IsNullOrEmpty( _appModel.Prompt))
+            {
+                _appModel.Prompt = 
+@"使用<data></data>标记的内容作为你的知识：
+<data>
+{{$doc}}
+</data>
+--------------------------
+回答要求：
+- 如果你不清楚答案，你需要澄清
+- 避免提及你是从<data></data>获取的知识
+- 保持答案与<data></data>众描述一致
+- 使用Markdown语法优化回答格式。
+- 如果Markdown有图片则正常显示
+--------------------------
+
+历史聊天记录:{{ConversationSummaryPlugin.SummarizeConversation $history}}
+--------------------------
+用户问题: {{$questions}}";
+            }
+
+            if (value == "chat")
+            {
+                _appModel.Prompt = "";
+            }
+        }
     }
 
 }
