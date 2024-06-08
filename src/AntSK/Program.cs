4f350081dd18e9c6f6c36e6eb50bd5a3847ff412
zyxucp <286513187@qq.com>
2024-06-08 11:23:02 +0800

    update llamasharp

--- src/AntSK/Program.cs
+++ src/AntSK/Program.cs
@@ -84,7 +84,7 @@ builder.Services.AddBackgroundTaskBroker().AddHandler<ImportKMSTaskReq, BackGrou
     if (LLamaSharpOption.RunType.ToUpper() == "CPU")
     {
         NativeLibraryConfig
-           .Instance
+           .All
            .WithCuda(false)
            .WithLogCallback((level, message) => {
                logger.LogInformation($"[llama {level}]: {message.TrimEnd('\n')}");
@@ -93,12 +93,11 @@ builder.Services.AddBackgroundTaskBroker().AddHandler<ImportKMSTaskReq, BackGrou
     else if (LLamaSharpOption.RunType.ToUpper() == "GPU")
     {
         NativeLibraryConfig
-        .Instance
+        .All
         .WithCuda(true)
         .WithLogCallback((level, message) => {
             logger.LogInformation($"[llama {level}]: {message.TrimEnd('\n')}");
-         })
-        .WithAvx(NativeLibraryConfig.AvxLevel.Avx);
+         });
     }
 
 
